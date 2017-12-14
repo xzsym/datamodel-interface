a3670704101c5026c3afb6a9d1dea7a6dd216140
xzsym <xiaoyu.zhang@symphony.com>
2017-12-13 18:10:31 -0800

    update interface to address review feedbacks

--- base.ts
+++ base.ts
@@ -21,27 +21,19 @@ export interface CollectionCallback<T> {
     (models: T):void;
 }
 
-export interface DeferredModel {
-    set(name: string, value: any):DeferredModel;
-    save():void;
-}
-
 /**
  * Model defines the common interfaces that read, update, and listen
  * to the changes of data
  */
 export interface Model {
     onChange(eventName: ModelEvent, prop: string, changeFunction: ModelCallback<Model>):void;
-    get(name: string): any;
-    set(name: string, val: any):void;
-    batchUpdate(): DeferredModel;
-    toJSON():Object;
-    toImmutableJS():Object;
+    update(modelProps:{[key: string]: any}): Promise<Model>;
+    toJSON(): Object;
+    toImmutableJS(): Object;
 }
 
 export interface Collection<T> {
     size(): number;
-    get(id: string): T | null;
     add(item: T):void;
     delete(id: string):boolean;
     onChange(eventName: CollectionEvent, callback: CollectionCallback<T>):void;
--- interfaces.ts
+++ interfaces.ts
@@ -1,18 +1,39 @@
 
 import { Model, Collection } from './base';
 
+enum PersistanceStatus {
+    CREATED = "CREATED",
+    CREATING = "CREATING",
+    FAILED = "FAILED",
+}
+
+enum MessageType {
+    MAESTRO = "MAESTRO",
+    SOCIALMESSAGE = "SOCIALMESSAGE"
+}
+
+export interface WritableMessageProps {
+}
 
 export interface Message extends Model {
+    readonly id: string;
     clientId: string;
     streamId: string;
     ingestionDate: number;
+    messageType: string;
     // dateStamp: number;
-    // messageType: string;
-    // persistenceStatus: string;    
+    // persistenceStatus: string;
+    update(modelProps:WritableMessageProps): Promise<Message>;
+}
+
+enum MaestroEvent {
+    CREATE_ROOM = "CREATE_ROOM",
+    JOIN_ROOM = "JOIN_ROOM",
 }
 
 export interface MestroMessage extends Message {
-    event: string;
+    event: MaestroEvent;
+    maestroObject: Object; //TODO: define interface for it
 }
 
 export interface SocialMessage extends Message {
@@ -25,45 +46,188 @@ export interface SocialMessage extends Message {
     // format: string;
     // ignoreDLPWarning: boolean;
     // images: object;
-    // isChime: boolean;
-    // isHighlighted: boolean;
-    // isReadByMe: boolean;
-    // isReadByOthers: boolean;
-    // isSuppressed: boolean;
+    isChime: boolean;
+    isHighlighted: boolean;
+    isReadByMe: boolean;
+    isReadByOthers: boolean;
+    isSuppressed: boolean;
     // jsonMedia: any[];
     // oboDelegateId: string | null;
     // semVersion: string;
     // shareMessageId: string | null;
     // smallImage: string | null;
     // socialStatus: object;
-    // text: string;
-    // userId: number;
-    // userPrettyName: string;
-    // userVerifiedForBadge: boolean;
+    text: string;
+    userId: number;
+    userPrettyName: string;
+    userVerifiedForBadge: boolean;
 }
 
 export interface WallPost extends Message {
-
+    like(): Promise<WallPost>;
+    share(comment:string): Promise<WallPost>;
 }
 
-export interface Attachment extends Model {}
+enum ImageType {
+    JPEG = "image/jpeg",
+    PNG = "image/png",
+}
+
+enum AttachmentStatus {
+    NOT_DOWNLOADED = "NOT_DOWNLOADED",
+    DOWNLOADED = "DOWNLOADED"
+}
+
+export interface Attachment extends Model {
+    fileId: string;
+    encrypted: boolean;
+    name: string;
+    images: { [key: string]: string };
+    width: number;
+    height: number;
+    orientation: number | null;
+    canPreview: boolean;
+    sizeInBytes: number;
+    contentType: ImageType;
+    isCollapsed: boolean;
+    thumbnailBlob: string;
+    previewBlob: string;
+    statue: AttachmentStatus;
+    previewStatus: AttachmentStatus;
+}
 
 interface NewMessageCallback {
     (message: Message): void;
 }
 
-export interface Stream extends Model {
-    getMessages(from: number, count: number): Array<SocialMessage>;
-    getMembers(): Collection<User>;
-    sendReceipt(messages:Message[]): Promise<object>;
+enum StreamType {
+    CHATROOM = "CHATROOM",
+    IM = "IM/MIM",
+}
 
+export interface StreamSettings {
+    default: boolean;
+    notificationType: string;
+    showToast: boolean;
+    blinkToast: boolean;
+    persistToast: boolean;
+    playSound: boolean;
+    toastColor: string;
+    // audioBridgeNumber: string | null;
+    // accessCode: string | null;
+    // emailDelivery: string | null;
+    xpodDisclaimerCompleted: boolean;
+    alias: string;
+    aliasColor: string;
+    // applyToAlert: boolean;
+    // applyToBackground: boolean;
+}
+
+export interface Stream extends Model, StreamSettings {
+    readonly id: string;
+    type: StreamType;
+    isCrossPod: boolean;
+    streamSettings: StreamSettings;
+
+    getMessages(from: number | null, to: number | null, count: number): Promise<Message[]>;
+    getMembers(): Promise<Collection<User>>;
+
+    update(settings: StreamSettings): Promise<Stream>;
+    sendReceipt(messages:Message[]): Promise<object>;
     uploadAttachment(attachment:Attachment): Promise<Attachment>;
-    sendMessage(): Promise<Message>;
+    sendMessage(message:Message): Promise<Message>;
+
     onNewMessage(callback:NewMessageCallback): void;
 }
 
+export interface IMStream extends Stream {
+    type: StreamType.IM;
+}
+export interface RoomStream extends Stream {
+    type: StreamType.CHATROOM;
+}
+
+enum RuleType {
+    KEYWORD = "KEYWORD"
+}
+
+export interface Rule {
+    // attributes: string | null;
+    // connectorId: string;
+    createDate: number;
+    definitionType: RuleType;
+    id: string;
+    // imageUrl: string | null;
+    // imageUrlSmall: string | null;
+    text: string;
+}
+
+export interface FilterRoleGroup {
+    operator: string;
+    rules: Rule[];
+}
+
+export interface Filter {
+    readonly id: string;
+    name: string;
+
+    getMessages(to: number, count: number): Promise<SocialMessage[]>;
+}
+
+export interface Mention extends Filter {
+}
+
+export interface Signal extends Filter {
+    roleGroup: FilterRoleGroup;
+    createDate: number;
+}
+
+enum PresenceStatus {
+    Avaialable = "Available",
+    Away = "Away",
+    InAMeeting = "In a meeting",
+    BeRightBack = "Be right back",
+    OffWork = "Off work",
+    Busy = "Busy",
+    OutOfOffice = "Out of office"
+}
+
+export interface PrescenceUpdateCallback {
+    (status: PresenceStatus):void;
+}
+
+export interface Entitlement {
+    isExternalIMEnabled: boolean;
+    isExternalRoomEnabled: boolean;
+    canJoinMultiLateralRoom: boolean;
+    canCreateMultiLateralRoom: boolean;
+}
+
 export interface User extends Model {
+    readonly id: number;
+    active: boolean;
+    companyName: string;
+    defaultImage: string;
+    entitlements: Entitlement[];
+    firstName: string;
+    followeeCount: number;
+    isCrossPod: boolean;
+    largeImage: string;
+    lastName: string;
+    location: string;
+    mediumImage: string;
+    prettyName: string;
+    // profileData: object;
+    // profileThreadId: string;
+    screenName: string;
+    smallImage: string;
+    title: string;
+    verifiedForBadge: boolean;
+
     getWallPosts(): Collection<WallPost>;
+    getRooms(): Promise<RoomStream[]>;
+    getSignals(): Promise<Signal[]>;
+    onPresenceUpdated(callback:PrescenceUpdateCallback):void;
 }
 
 // ***** Stores *****
@@ -71,31 +235,29 @@ export interface Store<T> {
     get(id: string): T | null;
 }
 
-export interface StoreDataCallback<T> {
-    (data: T, store: Store<T>): void;
-}
+// export interface StoreDataCallback<T> {
+//     (data: T, store: Store<T>): void;
+// }
 
-interface StreamDataListener<T> {
-    onReceiveData(eventName: string, callback:StoreDataCallback<T>): void;
-}
+// interface StreamDataListener<T> {
+//     onReceiveData(eventName: string, callback:StoreDataCallback<T>): void;
+// }
 
-export interface MessageStore extends Store<Message>, StreamDataListener<Message> {
-    getMessage(id: string): Message | null;
-}
+// export interface MessageStore extends Store<Message>, StreamDataListener<Message> {
+//     getMessage(id: string): Message | null;
+// }
 
 export interface UserStore extends Store<User> {
-    getUser(id: string): User | null;
+    getUser(id: string): Promise<User | null>;
+    findUser(name:string): Promise<User[]>;
 }
 
 export interface StreamStore extends Store<Stream> {
-    messageStore: MessageStore;
-    userStore: UserStore;
     getStream(id: string): Stream | null;
-    addStream(stream: Stream): void;
+    addStream(stream: Stream): Promise<Stream>;
 }
 
 // ****** misc *******
 export interface ApplicationSettings {
     setTheme(): void;
 }
-
